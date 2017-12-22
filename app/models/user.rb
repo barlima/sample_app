f31ff38d73488902bc02064a344faee76970843f
Bartek <bartekperucki@gmail.com>
2017-12-22 15:15:39 +0100

    Till 12.3.3

--- app/models/user.rb
+++ app/models/user.rb
@@ -23,6 +23,7 @@ class User < ApplicationRecord
 
   attr_accessor :remember_token
   attr_accessor :activation_token
+  attr_accessor :reset_token
 
   # Remembers a user in the database for use in persistent sessions.
   def remember
@@ -50,6 +51,22 @@ class User < ApplicationRecord
   # Sends activation email.
   def send_activation_email
     UserMailer.account_activation(self).deliver_now
+  end
+
+  # Sets the password reset attributes.
+  def create_reset_digest
+    self.reset_token = User.new_token
+    update_attribute(:reset_digest, User.digest(reset_token))
+    update_attribute(:reset_sent_at, Time.zone.now)
+  end
+
+  # Sends password reset email.
+  def send_password_reset_email
+    UserMailer.password_reset(self).deliver_now
+  end
+
+  def password_reset_expired?
+    reset_sent_at < 2.hours.ago
   end
 
   # Public methods
