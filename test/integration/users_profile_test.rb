1ee79efac76a1f5a27e65d4ba1af3362c07beca2
barlima <bartekperucki@gmail.com>
2017-12-27 17:04:51 +0100

    Add user following

--- test/integration/users_profile_test.rb
+++ test/integration/users_profile_test.rb
@@ -5,6 +5,7 @@ class UsersProfileTest < ActionDispatch::IntegrationTest
 
   def setup
     @user = users(:michael)
+    @other_user = users(:archer)
   end
 
   test "profile display" do
@@ -18,5 +19,21 @@ class UsersProfileTest < ActionDispatch::IntegrationTest
     @user.microposts.paginate(page: 1).each do |micropost|
       assert_match micropost.content, response.body
     end
+  end
+
+  test "profile stats" do
+    log_in_as(@user)
+    assert_redirected_to @user
+    get root_path
+    assert_match "following", response.body
+    assert_match "followers", response.body
+    get user_path(@other_user)
+    assert_match "following", response.body
+    assert_match "followers", response.body
+    assert_select 'form input[value=Follow]'
+    @user.follow(@other_user)
+    @user.reload
+    get user_path(@other_user)
+    assert_select 'form input[value=Unfollow]'
   end
 end
